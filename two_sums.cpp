3a8c88f591b558c11e20232b88211377fbc158f6
Vedant Kumar <vedantvisoliya@gmail.com>
2024-10-06 19:05:49 +0530

    Update two_sums.cpp
    used vector

--- two_sums.cpp
+++ two_sums.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 
-int main(){
-    int nums[] = {1,4,2,3,5};
-    int nums_length = sizeof(nums) / sizeof(nums[0]);
-    // cout << nums_length;
-    int sum = 9;
-
-    for (int i = 0; i < (nums_length-1); i++){
+vector<int> twoSums(vector<int> &nums, int target){
+    vector<int> result;
+    int numsize = nums.size();
+    for (int i = 0; i < (numsize-1); i++){
         int j = i + 1;
-        for (j; j < nums_length; j++){
-            if ((nums[i]+nums[j]) == sum){
-                cout << "[" << i << "," << j <<"]";
-                break;
+        for (j; j < numsize; j++){
+            if ((nums[i]+nums[j]) == target){
+                result.push_back(i);
+                result.push_back(j);
+                return result;
             }
         }
+    }
+    return result;
+}
+
+int main(){
+    vector<int> numArray = {1,2,3,4,5};
+
+    vector<int> output = twoSums(numArray,9);
+    int output_size = output.size();
+    for (int i = 0; i < output_size; i++){
+        cout << output[i] << " ";
     }
     return 0;
 }
